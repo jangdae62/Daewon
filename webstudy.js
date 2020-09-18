008d3f98577285089d227ad327a30a8ddfd78086
Daewon <jangdae62@gmail.com>
2020-09-18 15:18:06 +0900

    hello

---
+++ webstudy.js
@@ -0,0 +1,11 @@
+'use strict'
+var vcanvas, ctx;
+var food= { x:null, y:null};//클래스랑 비슷한 느낌 struct라 불림
+var s;
+
+function Snake(){
+    this.x=10;
+}//생성자 함수 모든변수를 지역변수this로 지정 함수안에서만 지정변수값으로 치환
+function init(){
+    
+}
